2fd2a432b70809340a8ce506bd62af4be4dd7e41
Anna Maria <anna.maria.tammin@ut.ee>
2022-11-23 01:42:22 +0200

    styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,61 +3,19 @@
     <ul id="nav-bar">
       <li class="nav-bar-element">
         <router-link class="nav-bar-link" to="/">Home</router-link>
+      </li>
+      <li class="nav-bar-element">
         <router-link class="nav-bar-link" to="/signup">Signup</router-link>
-
       </li>
       <!-- <li class="nav-bar-element"><a href="addPost.html" class="nav-bar-link">Add post</a></li> -->
-      <div class="profile-img dropdown">
-        <img src="res/img/profile_photo.png" alt="Profile photo" style="width: 60px" onclick="dropdown()"
-          class="dropbtn">
+      <div class="profile-img">
+        <img src="res/img/profile_photo.png" alt="Profile photo" style="width: 60px">
       </div>
     </ul>
-    <div id="picDropdown" class="dropdown-content">
-      <a href="#">Cat Cattington</a>
-      <a href="#">fluffypaws@me.ow</a>
-      <a href="#">Logout</a>
-    </div>
   </header>
 </template>
 
 <style scoped>
-.dropdown-content a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  border-radius: 10px;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  right: 5px;
-  background-color: #f1f1f1;
-  min-width: 160px;
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-  z-index: 1;
-  border-radius: 10px;
-  width: min-content;
-  text-align: left;
-}
-
-.dropdown-content a:hover {
-  background-color: #ddd;
-}
-
-.dropbtn {
-  cursor: pointer;
-  padding: 5px;
-  border-radius: 5px
-}
-
-/* Dropdown button on hover & focus */
-.dropbtn:hover,
-.dropbtn:focus {
-  background-color: lightgray;
-  border-radius: 10px;
-}
 
 #nav-bar {
   list-style-type: none;
@@ -70,7 +28,6 @@
 
 .nav-bar-element {
   float: left;
-
   font-size: 25px;
 }
 
@@ -84,10 +41,6 @@
 
 .nav-bar-link:hover {
   background-color: darkgrey;
-  border-radius: 10px;
-}
-
-.flex-item {
   border-radius: 10px;
 }
 
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,20 +1,19 @@
-
-
 <template>
-  <div class="center">
-    <div id="signup"></div>
-    <form id="app" @submit.stop.prevent="handleSubmit" action="/" method="post" novalidate="false">
+  <div class="container">
+  <form class="flex-item" id="app" @submit.stop.prevent="handleSubmit" action="/" method="post" novalidate="false">
+    <div id = "email">
       <p>Email:</p>
-      <input v-model="message" placeholder="Click to edit" type="email"/>
+      <input v-model="message" placeholder="email" type="email"/>
+    </div>
 
-      <p>Password:</p>
-      <input v-model="message" placeholder="Click to edit" />
-      
-      <p>
-        <input type="submit" value="Submit">
-      </p>
-      
-    </form>
+    <div id ="password">
+      <p>Password:</p >
+      <input v-model="message" placeholder="password" />
+    </div>
+    <p>
+      <input class="submit-button" type="submit" value="Submit">
+    </p>
+  </form>
   </div>
 </template>
 
@@ -29,8 +28,50 @@ const handleSubmit = (() => {
 
 
 <style scoped>
-.center {
-  margin-left: 10vw;
-  margin-right: 10vw;
+
+.flex-item {
+  border-radius: 10px;
 }
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-grow: 1;
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+
+a {
+  text-decoration: none;
+  text-align: center;
+}
+
+.container form {
+  background-color: dimgrey;
+  display: flex;
+  flex-direction: column;
+  width: max-content;
+  padding: 10px 10px 10px 10px;
+}
+
+input {
+  background-color: lightgray;
+  margin: 5px;
+  padding: 5px;
+  border-radius: 8px;
+}
+
+@media (max-width: 450px) {
+  .container form {
+    flex-grow: 1;
+    width: 75%;
+  }
+}
+
 </style>
